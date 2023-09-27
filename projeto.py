7a5b3ca0888b19a954d404dbad23c25cd7fedf85
willis <willis.v.santos@gmail.com>
2023-09-27 17:28:49 -0300

    atualização na branch local willislocal

--- projeto.py
+++ projeto.py
@@ -4,3 +4,4 @@ versao alterada no github na novabranch
 versao alterada no git local novabranchlocal
 Willis Vieira Santos
 versao alterada no github na branchwillis
+versao alterada no git local com a willislocal
